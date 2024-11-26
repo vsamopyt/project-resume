fbf404817f1df6196e8e241f97f631e738fdf997
vsamopyt <v.samopyt14@gmail.com>
2024-11-26 01:09:48 +0100

    the same problem

--- script/script.js
+++ script/script.js
@@ -1,5 +1,5 @@
 import {setupScrollToUpBtnListeners}  from "./scroll-to-up-btn.js";
 import {setupModalEventListeners} from "./modal-window.js";
 
-setupModalEventListeners()
-setupScrollToUpBtnListeners()
+// setupModalEventListeners()
+// setupScrollToUpBtnListeners()
